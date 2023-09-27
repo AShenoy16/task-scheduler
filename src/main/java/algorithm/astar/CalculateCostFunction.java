51f78db30e3ac6ea65ea455586a82a710fc9cd17
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-09-27 22:13:07 +1300

    add method to set bottom level values

--- src/main/java/algorithm/astar/CalculateCostFunction.java
+++ src/main/java/algorithm/astar/CalculateCostFunction.java
@@ -1,9 +1,38 @@
 package algorithm.astar;
 
-public class CalculateCostFunction {
+import model.Graph;
+import model.Node;
 
-    public void getBottomLevel(){}
-    public void setBottomLevel(){}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class CalculateCostFunction {
+    private Graph graph;
+    public static HashMap<Node,Integer> bottomLevelMap = new HashMap<>();
+
+    public void getBottomLevelMap(){}
+    public int setBottomLevelMap(Node node){
+        for (Node child : graph.getChildrenNodes(node)) {
+            // Recursive call to start from exit nodes
+            setBottomLevelMap(child);
+        }
+
+        // Get maximum bottom level value out of children nodes
+        int maxChildBottomLevel = graph.getChildrenNodes(node).stream()
+                .mapToInt(Node::getVal)
+                .max()
+                .orElse(0);
+
+        // Calculate bottom level value
+        int bottomLevel = maxChildBottomLevel + node.getVal();
+
+        // Save bottom level value to map
+        bottomLevelMap.put(node,bottomLevel);
+
+        return bottomLevel;
+    }
+
     public void getCostFunction(){}
 
     // this gets the cost function
