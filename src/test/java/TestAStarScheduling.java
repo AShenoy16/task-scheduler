3e446db12c0ceb222559297f1777e22da2e2758d
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-09-30 13:55:05 +1300

    fix test file names

--- src/test/java/TestAStarScheduling.java
+++ src/test/java/TestAStarScheduling.java
@@ -35,28 +35,28 @@ public class TestAStarScheduling {
     }
     @Test
     public void TestAStarTwoProcessorsNodes8() throws IOException {
-        Graph g = IOHandler.readDot(directory + "Nodes_7_OutTree.dot");
+        Graph g = IOHandler.readDot(directory + "Nodes_8_Random.dot");
         AstarScheduler scheduler = new AstarScheduler();
         var schedule = scheduler.run(g, 2);
         assertEquals(581, schedule.getCost());
     }
     @Test
     public void TestAStarFourProcessorsNodes8() throws IOException {
-        Graph g = IOHandler.readDot(directory + "Nodes_7_OutTree.dot");
+        Graph g = IOHandler.readDot(directory + "Nodes_8_Random.dot");
         AstarScheduler scheduler = new AstarScheduler();
         var schedule = scheduler.run(g, 4);
         assertEquals(581, schedule.getCost());
     }
     @Test
     public void TestAStarTwoProcessorsNodes9() throws IOException {
-        Graph g = IOHandler.readDot(directory + "Nodes_7_OutTree.dot");
+        Graph g = IOHandler.readDot(directory + "Nodes_9_SeriesParallel.dot");
         AstarScheduler scheduler = new AstarScheduler();
         var schedule = scheduler.run(g, 2);
         assertEquals(55, schedule.getCost());
     }
     @Test
     public void TestAStarFourProcessorsNodes9() throws IOException {
-        Graph g = IOHandler.readDot(directory + "Nodes_7_OutTree.dot");
+        Graph g = IOHandler.readDot(directory + "Nodes_9_SeriesParallel.dot");
         AstarScheduler scheduler = new AstarScheduler();
         var schedule = scheduler.run(g, 4);
         assertEquals(55, schedule.getCost());
@@ -64,14 +64,14 @@ public class TestAStarScheduling {
 
     @Test
     public void TestAStarTwoProcessorsNodes10() throws IOException {
-        Graph g = IOHandler.readDot(directory + "Nodes_7_OutTree.dot");
+        Graph g = IOHandler.readDot(directory + "Nodes_10_Random.dot");
         AstarScheduler scheduler = new AstarScheduler();
         var schedule = scheduler.run(g, 2);
         assertEquals(50, schedule.getCost());
     }
     @Test
     public void TestAStarFourProcessorsNodes10() throws IOException {
-        Graph g = IOHandler.readDot(directory + "Nodes_7_OutTree.dot");
+        Graph g = IOHandler.readDot(directory + "Nodes_10_Random.dot");
         AstarScheduler scheduler = new AstarScheduler();
         var schedule = scheduler.run(g, 4);
         assertEquals(50, schedule.getCost());
@@ -79,14 +79,14 @@ public class TestAStarScheduling {
 
     @Test
     public void TestAStarTwoProcessorsNodes11() throws IOException {
-        Graph g = IOHandler.readDot(directory + "Nodes_7_OutTree.dot");
+        Graph g = IOHandler.readDot(directory + "Nodes_11_OutTree.dot");
         AstarScheduler scheduler = new AstarScheduler();
         var schedule = scheduler.run(g, 2);
         assertEquals(350, schedule.getCost());
     }
     @Test
     public void TestAStarFourProcessorsNode11() throws IOException {
-        Graph g = IOHandler.readDot(directory + "Nodes_7_OutTree.dot");
+        Graph g = IOHandler.readDot(directory + "Nodes_11_OutTree.dot");
         AstarScheduler scheduler = new AstarScheduler();
         var schedule = scheduler.run(g, 4);
         assertEquals(227, schedule.getCost());
