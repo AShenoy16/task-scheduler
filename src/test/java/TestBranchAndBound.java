73f570fa03114a1e1dfe0d51ef5d0a6ff6de859c
Sli521 <sli521@aucklanduni.ac.nz>
2023-09-30 23:54:41 +1300

    test: node11 test cases for BNB uncommented

--- src/test/java/TestBranchAndBound.java
+++ src/test/java/TestBranchAndBound.java
@@ -74,20 +74,20 @@ public class TestBranchAndBound {
         assertEquals(50, schedule.getShortestPath());
     }
 
-//    @Test
-//    public void TestBnBTwoProcessorsNodes11() throws IOException {
-//        Graph g = IOHandler.readDot(directory + "Nodes_11_OutTree.dot");
-//        BranchAndBound scheduler = new BranchAndBound();
-//        Schedule schedule = scheduler.run(g, 2);
-//        assertEquals(350, schedule.getShortestPath());
-//    }
-//    @Test
-//    public void TestBnBFourProcessorsNode11() throws IOException {
-//        Graph g = IOHandler.readDot(directory + "Nodes_11_OutTree.dot");
-//        BranchAndBound scheduler = new BranchAndBound();
-//        Schedule schedule = scheduler.run(g, 4);
-//        assertEquals(227, schedule.getShortestPath());
-//    }
+    @Test
+    public void TestBnBTwoProcessorsNodes11() throws IOException {
+        Graph g = IOHandler.readDot(directory + "Nodes_11_OutTree.dot");
+        BranchAndBound scheduler = new BranchAndBound();
+        Schedule schedule = scheduler.run(g, 2);
+        assertEquals(350, schedule.getShortestPath());
+    }
+    @Test
+    public void TestBnBFourProcessorsNode11() throws IOException {
+        Graph g = IOHandler.readDot(directory + "Nodes_11_OutTree.dot");
+        BranchAndBound scheduler = new BranchAndBound();
+        Schedule schedule = scheduler.run(g, 4);
+        assertEquals(227, schedule.getShortestPath());
+    }
 
     @Test
     public void TestBNBOneProcessorsExample() throws IOException {
