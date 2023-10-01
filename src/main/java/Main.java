bb622d76068963e81f57cdfdab34d7ad578eedbe
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-10-01 13:20:53 +1300

    add main method execution
    This currently assumes only two arguments are passed input filename and number of processors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,28 @@
+import algorithm.astar.AstarScheduler;
+import io.IOHandler;
 import model.Graph;
+import model.Schedule;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
-//        Graph graph = io.IOHandler.readDot("Nodes_7_OutTree.dot");
-//        System.out.println(graph);
+        if (args == null || args.length < 2) {
+            throw new RuntimeException("InputFileName or numProcessors arguments not supplied");
+        }
+        String inputFileName = args[0];
+        String outputFileName = inputFileName + "-output.dot";
+        int numProcessors = Integer.parseInt(args[1]);
+
+        System.out.println("Starting schedule creation...");
+
+
+        IOHandler io = new IOHandler();
+        Graph graph = io.readDot(inputFileName);
+        AstarScheduler scheduler = new AstarScheduler();
+        Schedule schedule = scheduler.run(graph, numProcessors);
+
+
+        io.writeDot(schedule, outputFileName);
     }
 }
--- src/main/java/io/Main.java
+++
@@ -1,8 +0,0 @@
-package io;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Starting schedule creation...");
-    }
-
-}
