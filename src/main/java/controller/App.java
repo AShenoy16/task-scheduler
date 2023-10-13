5117d0cd1406e8e7fb68ae3612401ee1f7756b10
Sli521 <sli521@aucklanduni.ac.nz>
2023-10-13 13:46:28 +1300

    style: added styling to visualisation and resizing GUI

--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/visualisation.fxml"));
-        scene = new Scene(fxmlLoader.load(), 770, 580);
+        scene = new Scene(fxmlLoader.load(), 1200, 700);
         this.visualisationController = fxmlLoader.getController();
         stage.setTitle("Scheduling Algorithm Visualisation");
         stage.setScene(scene);
