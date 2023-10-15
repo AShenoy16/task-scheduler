9f45f781be1a57674ae4b08f7a9fb31aae4f40b3
Aditya Shenoy <97868877+AShenoy16@users.noreply.github.com>
2023-10-15 16:33:54 +1300

    feat/styled edges in node graph
    feat/styled edges in node graph

--- src/main/java/controller/VisualisationController.java
+++ src/main/java/controller/VisualisationController.java
@@ -198,7 +198,8 @@ public class VisualisationController {
         for (int i = 0; i < edges.length; i++) {
             for (int j = 0; j < edges.length; j++){
                 if (edges[i][j] != 0) {
-                    graphS.addEdge(i +","+j, i, j);
+                    org.graphstream.graph.Edge edge = graphS.addEdge(i + "," + j, i, j);
+                    edge.setAttribute("ui.style", "fill-color: #EFEFEF; size: 8px;");
                 }
             }
         }
